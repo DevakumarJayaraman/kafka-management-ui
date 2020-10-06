5860d444f9e4d5e37019bb379126dfc3ea93013c
Vignesh Nagarajan <nvignesh86@gmail.com>
2020-10-07 00:31:01 +0800

    Moving to static files to webapp

---
+++ src/main/webapp/precache-manifest.c659ea2c5fa83017d855b35aa3e64bfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163a96846150a1c1265031a2b70995ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07dd68a83a9d685aa4b9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25ded28e43c097b7c3a6",
+    "url": "/static/js/2.31fdd18a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.31fdd18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07dd68a83a9d685aa4b9",
+    "url": "/static/js/main.76e42511.chunk.js"
+  },
+  {
+    "revision": "7712bd1e484b00c71a57",
+    "url": "/static/js/runtime-main.dc3ab591.js"
+  }
+]);
